46fc0752136a03b9ec4529c96a8c3b67491957d6
tarunmitra <tarunmitra26@gmail.com>
2021-04-07 15:42:03 +0600

    first commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
-  const productsCollection = client.db("superShop").collection("product");
+  const productsCollection = client.db("superShop").collection("products");
   const checkOutCollection = client.db("superShop").collection("checkOut");
 
   app.get('/products', (req, res) => {
